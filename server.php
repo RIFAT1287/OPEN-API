f460d8054daec552abaabfea815a2524a8e2930c
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2024-11-10 12:21:43 +0600

    Update server.php

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ use OpenSwoole\Http\Server;
 use OpenSwoole\Http\Request;
 use OpenSwoole\Http\Response;
 
-$server = new OpenSwoole\Http\Server("0.0.0.0", 9501);
+$server = new OpenSwoole\Http\Server("0.0.0.0");
 
 $server->on("start", function (Server $server) {
     echo "OpenSwoole http server is started at http://127.0.0.1:9501\n";
