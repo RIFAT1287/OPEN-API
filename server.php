4d594902e4af485684fae95b2d2dfa292738c7a2
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2024-11-10 12:47:22 +0600

    Update server.php

--- server.php
+++ server.php
@@ -3,24 +3,12 @@ use OpenSwoole\Http\Server;
 use OpenSwoole\Http\Request;
 use OpenSwoole\Http\Response;
 
-$server = new OpenSwoole\Http\Server("0.0.0.0", 80);
+$port = getenv("PORT") ?: 9501;  
 
-$server->on("start", function (Server $server) {
-    echo "OpenSwoole http server is started at http://127.0.0.1:9501\n";
-});
+$server = new OpenSwoole\HTTP\Server("0.0.0.0", $port);
 
-$server->on("request", function (Request $request, Response $response) {
-    $response->header("Content-Type", "application/json");
-    
-    if ($request->server['request_uri'] === '/balance') {
-        $balanceData = [
-            'balance' => 1000, // Example balance
-            'currency' => 'USD'
-        ];
-        $response->end(json_encode($balanceData));
-    } else {
-        $response->end(json_encode(["message" => "Hello World"]));
-    }
+$server->on("request", function ($request, $response) {
+    $response->end("Hello from OpenSwoole on Render!");
 });
 
 $server->start();
